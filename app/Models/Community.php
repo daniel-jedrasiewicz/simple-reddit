98ad9f9100728e55610d58f4d54030149332db2f
daniel-jedrasiewicz <jedrasiewicz.daniel@gmail.com>
2023-03-04 16:54:27 +0100

    add slug to communities url

--- app/Models/Community.php
+++ app/Models/Community.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -9,9 +10,24 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Community extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Sluggable;
 
-    protected $fillable = ['user_id','name', 'description'];
+    protected $fillable = ['user_id','name', 'description','slug'];
+
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 
     public function topics(): BelongsToMany
     {
